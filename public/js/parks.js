b36ad54506fc59d80916c9b1bf772ccbf0f788f0
Kim Hagen <kimhagen@gmail.com>
2012-12-02 19:45:51 -0700

    icon improvments

--- public/js/parks.js
+++ public/js/parks.js
@@ -1,3 +1,47 @@
+function createPopupContent(item) {
+	var popupHeader = "<h5>" + item.Park_Name + "</h5>";
+	var popupLink = "<a href='http://maps.google.com/maps?saddr=" 
+		+ "35.0844,-106.6506" + 
+		"&daddr=" + item.lat + "," + item.long + "'>Get Directions</a>";
+	return popupHeader + popupLink;
+}
+
+
+function createLayerGroup(parks, condf) {
+	var filteredItems = jQuery.grep(parks, condf);
+	
+	var itemArray = [];
+	$.each(filteredItems, function(i, item){
+		var marker = L.marker([item.lat, item.long], {icon: greenIcon});
+		var content = createPopupContent(item);
+		marker.bindPopup(content);
+		itemArray.push(marker);
+	});
+	return L.layerGroup(itemArray);
+}
+
+function setupLayerClick(layerName, layer, map) {
+	$('#' + layerName).click(function(){
+		if (!map.hasLayer(layer)) {
+			layer.addTo(map);
+		}
+		else {
+			map.removeLayer(layer);
+		}
+	});
+}
+
+var greenIcon = L.icon({
+    iconUrl: 'images/urbanpark.png',
+    iconSize:     [32, 37], // size of the icon
+    iconAnchor:   [15, 37], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0,0] // point from which the popup should open relative to the iconAnchor
+});
+
+
+
+
+
 $(function () {
 	
 
@@ -9,79 +53,37 @@ $(function () {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
   }).addTo(map);
+	
+
 
   $.getJSON("data/CityParks.json", function(json) {
     console.log(json); // this will show the info it in firebug console
     var parks = json;
-
-
-    picnicParks = jQuery.grep(parks, function(element, index){
-     return element['Picnic_Tables'] > 0; // retain appropriate elements
-    });
-	
-  	var picnicArray = [];
-    $.each(picnicParks, function(i, item) {
-      var marker = L.marker([item.lat, item.long]);
-      marker.bindPopup(item.Park_Name);
-      picnicArray.push(marker);
-    });
-  	var picnic = L.layerGroup(picnicArray);
-
-   
-    playParks = jQuery.grep(parks, function(element, index){
-      return element['Play_Areas'] > 0  // retain appropriate elements
-    });
-
-  	var playArray = [];
-    $.each(playParks, function(i, item) {
-      var marker = L.marker([item.lat, item.long]);
-      marker.bindPopup(item.Park_Name);
-      playArray.push(marker);
-    });
-  	var playgrounds = L.layerGroup(playArray);
-	
-    tennisParks = jQuery.grep(parks, function(element, index){
-      return (element['Lit_Tennis_Courts'] > 0 || element['Unlit_Tennis_Courts'] > 0)  // retain appropriate elements
-    });
-	
-  	var tennisArray = [];
-    $.each(tennisParks, function(i, item) {
-      var marker = L.marker([item.lat, item.long]);
-      marker.bindPopup(item.Park_Name);
-      tennisArray.push(marker);
-    });
-  	var tennis = L.layerGroup(tennisArray);
-	
-	
-    $('#playgrounds').click(function(){
-			if (!map.hasLayer(playgrounds)) {
-    		playgrounds.addTo(map);
-			}
-			else {
-				map.removeLayer(playgrounds);
-			}
-			
-    });
-
-    $('#picnic').click(function(){
-			if (!map.hasLayer(picnic)) {
-    		picnic.addTo(map);
-			}
-			else {
-				map.removeLayer(picnic);
-			}
-
-    });
-	
-    $('#tennis').click(function(){
-			if (!map.hasLayer(tennis)) {
-    		tennis.addTo(map);
-			}
-			else {
-				map.removeLayer(tennis);
-			}
 		
-    });
+		var picnicLayer = createLayerGroup(parks, function(el) { return el['Picnic_Tables'] > 0; });
+		
+		var tennisLayer = createLayerGroup(parks, function(el) { return (el['Lit_Tennis_Courts'] > 0 || el['Unlit_Tennis_Courts'] > 0);});
+		
+		var playgroundLayer = createLayerGroup(parks, function(el) { return el['Play_Areas'] > 0; });
+		
+		var basketballLayer = createLayerGroup(parks, function(el) { return (el['Full_Basketball_Courts'] > 0 || el['Half_Basketball_Courts'] > 0);});
+	
+	  var swimLayer = createLayerGroup(parks, function(el) { return (el['Indoor_Pools'] > 0 || el['Outdoor_Pools'] > 0);});
+		
+		var runLayer = createLayerGroup(parks, function(el) { return el['Jogging_Paths'] > 0; });
+		
+		setupLayerClick("playgrounds", playgroundLayer, map);
+		
+		setupLayerClick("picnic", picnicLayer, map);
+		
+		setupLayerClick("tennis", tennisLayer, map);
+		
+		setupLayerClick("basketball", basketballLayer, map);
+		
+		setupLayerClick("swim", swimLayer, map);
+		
+		setupLayerClick("run", runLayer, map);
+	
   });
 	
 	// Site UI elements
